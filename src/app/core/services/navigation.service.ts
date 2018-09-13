5aebb1716f80f6ff6b77afd339cae6f3a0d112da
pcboby <pcboby@126.com>
2018-09-13 17:46:46 +0800

    update icon with mine

--- src/app/core/services/navigation.service.ts
+++ src/app/core/services/navigation.service.ts
@@ -10,95 +10,95 @@ export class NavigationService {
   private navmenu = [{
     id: '001',
     title: '基础数据',
-    icon: 'fa fa-search',
+    icon: 'pIcon pIcon-001',
     data: [{
       id: '001',
       title: '模型管理',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-001001',
     }, {
       id: '002',
       title: '专题管理',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-001002',
     }, {
       id: '003',
       title: '倾斜管理',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-001003',
     }]
   }, {
     id: '002',
     title: '漫游标注',
-    icon: 'fa fa-search',
+    icon: 'pIcon pIcon-002',
     data: [{
       id: '001',
       title: '三维漫游',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-002001',
     }, {
       id: '002',
       title: '标注',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-002002',
     }]
   }, {
     id: '003',
     title: '查询定位',
-    icon: 'fa fa-search',
+    icon: 'pIcon pIcon-003',
     data: [{
       id: '001',
       title: '点击查询',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-003001',
     }, {
       id: '002',
       title: '属性查询',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-003002',
     }]
   }, {
     id: '004',
     title: '分晰量算',
-    icon: 'fa fa-search',
+    icon: 'pIcon pIcon-004',
     data: [{
       id: '001',
       title: '距离量算',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-004001',
     }, {
       id: '002',
       title: '面积量算',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-004002',
     }, {
       id: '003',
       title: '高度量算',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-004003',
     }, {
       id: '004',
       title: '剖面分析',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-004004',
     }, {
       id: '005',
       title: '地形开挖分析',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-004005',
     }, {
       id: '006',
       title: '缓冲区分析',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-004006',
     }, {
       id: '007',
       title: '空间叠加分析',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-004007',
     }, {
       id: '008',
       title: '坡反分析',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-004008',
     }]
   }, {
     id: '005',
     title: '演进展示',
-    icon: 'fa fa-search',
+    icon: 'pIcon pIcon-005',
     data: [{
       id: '001',
       title: '洪水推演',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-005001',
     }, {
       id: '002',
       title: '淹没分析',
-      icon: 'fa fa-search'
+      icon: 'pIcon pIcon-005002',
     }]
   }];
 
