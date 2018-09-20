4a5ec81e449a7ecca86778dab0ac84cac05f9499
pcboby <54257013@qq.com>
2018-09-20 14:40:50 +0800

    update

--- src/app/views/map/components/basicData/render-mode/render-mode.component.ts
+++ src/app/views/map/components/basicData/render-mode/render-mode.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import untils from '../../../../../config/untils';
 
 @Component({
   selector: 'app-render-mode',
@@ -13,7 +14,16 @@ export class RenderModeComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-  pointRender() {}
+  /**
+   * functions
+   */
+  pointRender() {
+    // untils().ArkMap().test1('111');
+    // console.log(untils().ArkMapRef()['ArkScene']);
+            var url = 'http://' + window.location.host + '/assets/Data/Terrains/ChongQing.arkpro';
+            untils().ArkMapRef()['Initialize'](url);
+            untils().ArkMapRef()['ArkScene']['RenderMode'] = 0
+  }
   lineRender() {}
   polygonRender() {}
 
