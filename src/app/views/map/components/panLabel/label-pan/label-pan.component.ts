40d19e0fee582eed7b6929a9d10f70a3d5a5e168
pcboby <54257013@qq.com>
2018-09-20 23:47:43 +0800

    update

--- src/app/views/map/components/panLabel/label-pan/label-pan.component.ts
+++ src/app/views/map/components/panLabel/label-pan/label-pan.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import untils from '../../../../../config/untils';
 
 @Component({
   selector: 'app-label-pan',
@@ -13,14 +14,30 @@ export class LabelPanComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-  pointLabel() {}
-  polylineLabel() {}
-  polygonLabel() {}
-  rectangleLabel() {}
-  circleLabel() {}
-  ellipseLabel() {}
-  arcLabel() {}
-  sectorLabel() {}
+  pointLabel() {
+    untils().ArkMap().DrawPoint();
+  };
+  polylineLabel() {
+    untils().ArkMap().DrawPolyline();
+  };
+  polygonLabel() {
+    untils().ArkMap().DrawPolygon();
+  };
+  rectangleLabel() {
+    untils().ArkMap().DrawRect();
+  };
+  circleLabel() {
+    untils().ArkMap().DrawCircle();
+  };
+  ellipseLabel() {
+    untils().ArkMap().DrawEllipse();
+  };
+  arcLabel() {
+    untils().ArkMap().DrawArc();
+  };
+  sectorLabel() {
+    untils().ArkMap().DrawSector();
+  };
 
 
 }
