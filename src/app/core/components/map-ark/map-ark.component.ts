a82e9a1d553be9946c9fd32efc9197a1f1a54269
pcboby <pcboby@126.com>
2018-09-23 22:05:45 +0800

    update

--- src/app/core/components/map-ark/map-ark.component.ts
+++ src/app/core/components/map-ark/map-ark.component.ts
@@ -7,7 +7,10 @@ import {
 
 import * as _ from 'lodash';
 import untils from '../../../config/untils';
-import { environment } from '../../../../environments/environment';
+import {
+  environment
+} from '../../../../environments/environment';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-map-ark',
@@ -23,8 +26,46 @@ export class MapArkComponent implements OnInit, OnDestroy {
     // return true;
     return untils().IE() > 10;
   }
-  ngOnInit() {}
+
+  get hasArk() {
+    return detectPlugin();
+  }
+
+
+  ngOnInit() {
+    check();
+  }
   ngOnDestroy() {
     untils().ArkMap().UnInitialize();
   }
 }
+
+const detectPlugin = function () {
+  const pluginDiv = $(`<div id=" pluginDiv " style=" display: none "></div>`).appendTo(document.body);
+  // tslint:disable-next-line:max-line-length
+  pluginDiv.html(`<object style="position:relative" id="testArkObj" type="application/x-itst-activex" classid="CLSID:0BCAF84B-7570-4A96-9528-542A77E6606A">
+    <param name="_Version" value="65536">
+    <param name="_ExtentX" value="13229">
+    <param name="_ExtentY" value="13229">
+    <param name="_StockProps" value="0">
+  </object>`);
+  try {
+    if (document.all['testArkObj'].object === undefined) {
+      pluginDiv.remove(); // 删除pluginDiv及其所有的子元素
+      return false;
+    } else {
+      pluginDiv.remove(); // 删除pluginDiv及其所有的子元素
+      return true;
+    }
+  } catch (e) {
+    return false;
+  }
+};
+
+const check = function () {
+  if (detectPlugin()) {
+    console.log('该控件已经安装');
+  } else {
+    console.log('该控件未安装');
+  }
+};
--- src/app/views/map/components/performance/flood-deduction/flood-deduction.component.ts
+++ src/app/views/map/components/performance/flood-deduction/flood-deduction.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MapperService } from '../../../../../core/services';
+import untils from '../../../../../config/untils';
 
 @Component({
   selector: 'app-flood-deduction',
   templateUrl: './flood-deduction.component.html',
   styleUrls: ['./flood-deduction.component.css']
 })
-export class FloodDeductionComponent implements OnInit {
+export class FloodDeductionComponent implements OnInit, OnDestroy {
 
-  constructor(private mapperService: MapperService) { }
+  public viewMode = '';
+  public percent = 50;
+  // untils().ArkMap().FloodProc_StepIt()
 
-  ngOnInit() {
+  constructor(private mapperService: MapperService) {}
+
+  ngOnInit() {}
+  ngOnDestroy() {
+    untils().ArkMap().FloodProc_Close();
   }
+
+
+  play() {
+    this.viewMode = 'play';
+    untils().ArkMap().FloodProc_Init();
+    console.log('初始化完成');
+  }
+  pause() {
+    this.viewMode = 'pause';
+    untils().ArkMap().FloodProc_Pause();
+  }
+  continue () {
+    this.viewMode = 'play';
+    untils().ArkMap().FloodProc_ContinuePlay();
+  }
+  stop() {
+    this.viewMode = 'stop';
+    untils().ArkMap().FloodProc_stop();
+  }
+
 
 }
