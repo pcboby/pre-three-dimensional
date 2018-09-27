60fb41e0b0911443f49d1595f2be567accd6e5f2
pcboby <pcboby@126.com>
2018-09-27 13:20:33 +0800

    update

--- src/app/views/lanzhou/components/location/location-road/location-road.component.ts
+++ src/app/views/lanzhou/components/location/location-road/location-road.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location-road.component.css']
 })
 export class LocationRoadComponent implements OnInit {
-
+  data = [
+    {
+      title: 'Ant Design Title 1'
+    },
+    {
+      title: 'Ant Design Title 2'
+    },
+    {
+      title: 'Ant Design Title 3'
+    },
+    {
+      title: 'Ant Design Title 4'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
